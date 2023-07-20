83a31e146a1af72e1de6925ed128b409697e0016
hebertdelima13 <hebertdelima13@gmail.com>
2023-07-20 06:59:54 -0300

    add ngx-bootstrap

--- src/app/libs.data.ts
+++ src/app/libs.data.ts
@@ -231,4 +231,62 @@ export const LIBRARY_SUPPORT_DATA: LibrarySupport[] = [
       },
     },
   },
+   {
+    name: 'ngx-bootstrap',
+    npmUrl: 'https://www.npmjs.com/package/ngx-bootstrap',
+    githubUrl: 'https://github.com/valor-software/ngx-bootstrap',
+    versionSupport: {
+      '16': {
+        libraryVersion: '11.0.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v11.0.0',
+      },
+      '15': {
+        libraryVersion: '10.0.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v10.0.0',
+      },
+      '14': {
+        libraryVersion: '9.0.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v9.0.0',
+      },
+      '13': {
+        libraryVersion: '8.0.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v8.0.0',
+      },
+      '12': {
+        libraryVersion: '8.0.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v8.0.0',
+      },
+      '11': {
+        libraryVersion: '7.0.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v7.0.0',
+      },
+      '10': {
+        libraryVersion: '6.0.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v6.0.0',
+      },
+      '9': {
+        libraryVersion: '6.0.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v6.0.0',
+      },
+      '8': {
+        libraryVersion: '5.6.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v5.6.0',
+      },
+      '7': {
+        libraryVersion: '5.0.0',
+        support: true,
+        link: 'https://github.com/valor-software/ngx-bootstrap/releases/tag/v5.0.0',
+      },
+    },
+  },
+
 ];
