48f03a54dc71cd703407884b3d760be7acc0f6b6
Enea Jahollari <jahollarienea14@gmail.com>
2023-07-20 21:18:50 +0200

    feat: add redirect to ngx-libs.com

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+// redirect to https://ngx-libs.com if it's not that domain or localhost
+if (location.hostname !== 'ngx-libs.com' && location.hostname !== 'localhost') {
+  location.href = 'https://ngx-libs.com' + location.pathname;
+}
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
