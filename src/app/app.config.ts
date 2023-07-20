4e4b0924ebd22bab6263142b90fcfe5de3a44cd6
Ajit Panigrahi <ajitzero@users.noreply.github.com>
2023-07-21 03:36:00 +0530

    Remove unnecessary route file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideRouter(appRoutes)],
+  providers: [provideAnimations(), provideRouter([])],
 };
--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Route } from '@angular/router';
-import { AppComponent } from './app.component';
-
-export const appRoutes: Route[] = [
-  { path: '', component: AppComponent, pathMatch: 'full' },
-];
