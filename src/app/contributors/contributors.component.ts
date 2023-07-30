3db48355ce2252a629341b01f9c5792f37e92361
Enea Jahollari <jahollarienea14@gmail.com>
2023-07-30 23:13:23 +0200

    show contrib count

--- src/app/contributors/contributors.component.ts
+++ src/app/contributors/contributors.component.ts
@@ -10,7 +10,7 @@ import { ContributorsStore } from './store/contributors.store';
   selector: 'app-contributors',
   template: `
     <div class="contributors-container">
-      <h3 class="title">Contributors</h3>
+      <h3 class="title">{{ vm().contributors.length }} Contributors 🪄</h3>
       <ng-container *ngIf="vm() as vm">
         <ng-container *ngIf="vm.isLoading; else contributorsTemplate">
           <app-contributors-loader count="5" />
@@ -27,10 +27,14 @@ import { ContributorsStore } from './store/contributors.store';
             >
               <img
                 [ngSrc]="contributor.avatar_url"
-                width="32"
-                height="32"
+                loading="lazy"
+                width="40"
+                height="40"
                 [alt]="contributor.login"
               />
+              <span class="contributions-count">
+                {{ contributor.contributions }}
+              </span>
             </a>
           </div>
         </ng-template>
@@ -65,12 +69,30 @@ import { ContributorsStore } from './store/contributors.store';
 
       .contributor {
         display: block;
-        width: 2rem;
-        height: 2rem;
+        width: 2.5rem;
+        height: 2.5rem;
         border-radius: 4rem;
+        position: relative;
+
         img {
           object-fit: cover;
           border-radius: 4rem;
+        }
+
+        .contributions-count {
+          position: absolute;
+          bottom: -0.25rem;
+          right: -0.25rem;
+          background-color: #000;
+          border-radius: 50%;
+          width: 1.5rem;
+          height: 1.5rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 0.75rem;
+          font-weight: 600;
+          color: #fff;
         }
       }
     `,
